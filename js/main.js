4fd8ef126f8cb48bf174d30f2113fead7b763416
Janete Deucher <janetedeucher@gmail.com>
2021-07-07 15:41:13 -0300

    add js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+  $('.imagem-detalhada').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+
+  $('.tooltip').tooltipster({
+    theme: 'tooltipster-light'
+});
+
+
